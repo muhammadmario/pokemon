2199848482cfbd8399da4b31e23c03f965fd9ae4
Muhammad Mario <acernitro58@gmail.com>
2022-10-07 10:55:33 +0700

    menambah footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="h-10 bg-blue-500 text-white flex justify-center items-center">
+      Dibuat dengan ❤ oleh Mario
+    </footer>
+  );
+}
+
+export default Footer;
